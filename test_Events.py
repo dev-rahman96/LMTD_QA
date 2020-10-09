00472392c10d01e216dfdbe6146b37d01e69e219
Raymond Martinez <raymondmartinez@Raymonds-MBP.home>
2020-10-09 19:39:39 -0400

    Fork from events service

--- test_Events.py
+++ test_Events.py
@@ -1,28 +1,55 @@
 from flask import Flask, request
 from AWSManager import Events
-import pytest
  
- 
+event = Events()
 app = Flask(__name__)
-t1 = Events()
 
-@app.route("/event")
+
+@app.route("/create-event", methods=["POST"])
 def create():
-    t1.put(Event_name = "Presentation", Event_date = "October 3rd", Event_time = "2pm", User = "Class", Event_desc = "Presenting things", Event_image = "None", Event_location = "Zoom")
+    #event.put(Event_name = "trPresentation", Event_date = "October 3rd", Event_time = "2pm", User = "Class", Event_desc = "Presenting things", Event_image = "None", Event_location = "Zoom")
+    res = request.json
+    Event_name = res['Event_name']
+    Event_date = res['Event_date']
+    Event_time = res['Event_time']
+    User = res['User']
+    Event_desc = res['Event_desc']
+    Event_image = res['Event_image']
+    Event_location = res['Event_location']
+
+    event.check_if_event_exists(Event_name = Event_name, Event_date = Event_date, Event_time = Event_time, User = User, Event_desc= Event_desc, Event_image = Event_image, Event_location = Event_location)
     
-# @app.route("/event/viewing", methods= ["GET"])
-# def view_event():
-#     return 
+@app.route("/event-delete", methods=["POST"])
+def delete():
+    res = request.json
+    Event_name = res['Event_name']
+    
+    event.delete(Event_name = Event_name)
 
-# @app.route("/event/update")
-# def update_event():
-#     t1.put("Event_name", "Event_desc", "Date_time", "Price", "Address", "Virtual")
+#not done yet
+@app.route("/event-update", methods=["POST"]) 
+def update():
+    res = request.json
+    New_Event_name = res['New_Event_name']
+    New_Event_date = res['New_Event_date']
+    New_Event_time = res['New_Event_time']
+    New_User = res['New_User']
+    New_Event_desc = res['New_Event_desc']
+    New_Event_image = res['New_Event_image']
+    New_Event_location = res['New_Event_location']
 
+    event.update()
 
-# @app.route("/event/delete", methods=["POST"])
-# def delete_eventç():
-#      t1.put("Event_name", "Event_date", "Event_time", "User", "Event_desc", "Event_image", "Event_location")
+@app.route("/event-view", methods= ["GET"])
+def view_event():
+    res = event.view()
+    return res
 
+#still a work in progress
+@app.route("/event-rsvp", methods= ["GET"])
+def rsvp():
+    res = event.rsvp()
+    return res
 # @app.route("/event/rsvp", methods=["POST"])
 # def rsvp():
 #      t1.put("Event_name", "Event_date", "Event_time", "User", "Event_desc", "Event_image", "Event_location")
@@ -54,6 +81,6 @@ def test_createEvent():
 
 def test_eventRSVP():
         with app.test_client() as c:
-            response = c.get('/event/rsvp')
-            assert(response.status_code, 200)
+            response = c.get('/event-rsvp')
+            assert response.status_code == 200
 
